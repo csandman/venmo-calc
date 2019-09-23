229c3071b9f05850e655c8a17a119b0eff3a7e7d
csandman <csandvik@uvm.edu>
2019-09-23 17:44:22 -0400

    slightly changed content

--- content.js
+++ content.js
@@ -9,42 +9,38 @@
 //   document.querySelector('.moreButton').click();
 // }
 // content.js
-chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-    if( request.message === "clicked_browser_action" ) {
-      
-      clickMoreButtons();
-      // injectContainer();
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+    if (request.message === "clicked_browser_action") {
+        clickMoreButtons();
+        // injectContainer();
     }
-  }
-);
+});
 
 function injectContainer() {
-  console.log('new function')
-  var outerDiv = document.createElement("div");
-  outerDiv.setAttribute("style", "display:flex;");
+    console.log("new function");
+    var outerDiv = document.createElement("div");
+    outerDiv.setAttribute("style", "display:flex;");
 
-  var bodyDiv = document.createElement("div");
-  bodyDiv.setAttribute("style", "flex:4;");
-  bodyDiv.id = "wrap";
+    var bodyDiv = document.createElement("div");
+    bodyDiv.setAttribute("style", "flex:4;");
+    bodyDiv.id = "wrap";
 
-  var sideDiv = document.createElement("div");
-  sideDiv.setAttribute("style", "flex:1;background-color:grey");
-  sideDiv.innerText = "This is my new sidebar";
+    var sideDiv = document.createElement("div");
+    sideDiv.setAttribute("style", "flex:1;background-color:grey");
+    sideDiv.innerText = "This is my new sidebar";
 
-  // Move the body's children into this wrapper
-  while (document.body.firstChild)
-  {
-      bodyDiv.appendChild(document.body.firstChild);
-  }
+    // Move the body's children into this wrapper
+    while (document.body.firstChild) {
+        bodyDiv.appendChild(document.body.firstChild);
+    }
 
-  console.log(bodyDiv);
+    console.log(bodyDiv);
 
-  outerDiv.appendChild(bodyDiv);
-  outerDiv.appendChild(sideDiv);
+    outerDiv.appendChild(bodyDiv);
+    outerDiv.appendChild(sideDiv);
 
-  // Append the wrapper to the body
-  document.body.appendChild(outerDiv);
+    // Append the wrapper to the body
+    document.body.appendChild(outerDiv);
 }
 function clickNextButton() {
     //This will click the next button in the sequence which we desire.
@@ -54,67 +50,75 @@ function clickNextButton() {
     //  It returns false when there are no more buttons to click;
     //If there is a button in the "save" state, then we need to deal with it first and
     //  not move to the next "update" button until the "save" button changes to "saved".
-    if (document.querySelector('.moreButton')) {
-      // if (document.querySelector('.moreButton').innerText == "No more payments") {
-      //   return false;
-      // } else {
-      //   document.querySelector('.moreButton').click();
-      // }
-      if (document.querySelector('#profile_feed_no_stories').style.display != "none") {
+    if (document.querySelector(".moreButton")) {
+        // if (document.querySelector('.moreButton').innerText == "No more payments") {
+        //   return false;
+        // } else {
+        //   document.querySelector('.moreButton').click();
+        // }
+        window.scrollTo(0,document.body.scrollHeight);
+        if (
+            document.querySelector("#profile_feed_no_stories").style.display !==
+                "none" ||
+            document.querySelector(".moreButton").innerText.toLowerCase() ===
+                "no more payments"
+        ) {
+            return false;
+        } else {
+            document.querySelector(".moreButton").click();
+        }
+        return true;
+    } else {
         return false;
-      } else {
-        document.querySelector('.moreButton').click();
-      }
     }
-    return true;
+    
 }
 function clickMoreButtons() {
-  if(clickNextButton()) {
-      //Keep clicking buttons until there are none in the "update" or "save" states.
-      setTimeout(clickMoreButtons,100); //Call this function again in 200ms.
-  } else {
-    console.log('donclickingbuttons');
-    console.log(document.querySelector('.venmo-calc-results'));
-    // document.querySelector('.venmo-calc-loader').classList.add('hidden');
-    calcStats();
-  }
+    if (clickNextButton()) {
+        //Keep clicking buttons until there are none in the "update" or "save" states.
+        setTimeout(clickMoreButtons, 100); //Call this function again in 200ms.
+    } else {
+        console.log("donclickingbuttons");
+        console.log(document.querySelector(".venmo-calc-results"));
+        // document.querySelector('.venmo-calc-loader').classList.add('hidden');
+        calcStats();
+    }
 }
 function loadResultsFile() {
-  fetch(chrome.runtime.getURL('/results.html'))
-  .then(response => response.text())
-  .then(data => {
-    document.body.innerHTML += data;
-    // other code
-    // eg update injected elements,
-    // add event listeners or logic to connect to other parts of the app
-  }).catch(err => {
-    // handle error
-  });
+    fetch(chrome.runtime.getURL("/results.html"))
+        .then(response => response.text())
+        .then(data => {
+            document.body.innerHTML += data;
+            // other code
+            // eg update injected elements,
+            // add event listeners or logic to connect to other parts of the app
+        })
+        .catch(err => {
+            // handle error
+        });
 }
 function calcStats() {
-  var recievedArr = [...document.querySelectorAll('.bold.medium.green')];
-  recievedArr = recievedArr.map(el => parseFloat(el.innerText.slice(2)));
-  var sentArr = [...document.querySelectorAll('.bold.medium.red')];
-  sentArr = sentArr.map(el => parseFloat(el.innerText.slice(2)));
-  console.log(recievedArr,sentArr);
-  recievedTotal = recievedArr.reduce((a,b)=>a+b).toFixed(2);
-  sentTotal = sentArr.reduce((a,b)=>a+b).toFixed(2);
-  console.log(recievedTotal,sentTotal);
+    var recievedArr = [...document.querySelectorAll(".bold.medium.green")];
+    recievedArr = recievedArr.map(el => parseFloat(el.innerText.slice(2)));
+    var sentArr = [...document.querySelectorAll(".bold.medium.red")];
+    sentArr = sentArr.map(el => parseFloat(el.innerText.slice(2)));
+    console.log(recievedArr, sentArr);
+    recievedTotal = recievedArr.reduce((a, b) => a + b).toFixed(2);
+    sentTotal = sentArr.reduce((a, b) => a + b).toFixed(2);
+    console.log(recievedTotal, sentTotal);
 
-  var names = document.querySelectorAll('.m_five_t.p_ten_r');
-  console.log(names);
-  var me = document.querySelector('.rectBox .p_ten_t .bold').innerText;
-  console.log(me);
-  names = [...names].map(a => a.innerText);
-  console.log(names);
-  chargedArr = names.filter(a => a.includes('charged'))
-  console.log(chargedArr);
+    var names = document.querySelectorAll(".m_five_t.p_ten_r");
+    console.log(names);
+    var me = document.querySelector(".rectBox .p_ten_t .bold").innerText;
+    console.log(me);
+    names = [...names].map(a => a.innerText);
+    console.log(names);
+    chargedArr = names.filter(a => a.includes("charged"));
+    console.log(chargedArr);
 
-  loadResultsFile();
-  document.querySelector('.ui.modal').modal('show');
-  // paidArr = [...names].innterText.reduce(a => a.includes('paid'));
-  // chargedMe = chargedArr.reduce(a  => a.children[1] == me);
-  // console.log(chargedMe);
-
-
+    loadResultsFile();
+    document.querySelector(".ui.modal").modal("show");
+    // paidArr = [...names].innterText.reduce(a => a.includes('paid'));
+    // chargedMe = chargedArr.reduce(a  => a.children[1] == me);
+    // console.log(chargedMe);
 }
