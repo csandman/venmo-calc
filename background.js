2b122b9bdcc9404da5b0172675b1d857ccf4a0d9
csandman <csandvik@uvm.edu>
2019-09-23 22:43:42 -0400

    First fully working version

--- background.js
+++ background.js
@@ -1,10 +1,16 @@
-// background.js
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
 
-// Called when the user clicks on the browser action.
-chrome.browserAction.onClicked.addListener(function(tab) {
-  // Send a message to the active tab
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    var activeTab = tabs[0];
-    chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_browser_action"});
+'use strict';
+
+chrome.runtime.onInstalled.addListener(function() {
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+    chrome.declarativeContent.onPageChanged.addRules([{
+      conditions: [new chrome.declarativeContent.PageStateMatcher({
+        pageUrl: {hostEquals: 'venmo.com'},
+      })],
+      actions: [new chrome.declarativeContent.ShowPageAction()]
+    }]);
   });
 });
---
+++ collect_stats.js
@@ -0,0 +1,91 @@
+clickMoreButtons();
+
+function clickNextButton() {
+  window.scrollTo(0, document.body.scrollHeight);
+  if (document.querySelector(".moreButton")) {
+    if (
+      document.querySelector("#profile_feed_no_stories").style.display !==
+        "none" ||
+      document.querySelector(".moreButton").innerText.toLowerCase() ===
+        "no more payments"
+    ) {
+      return false;
+    } else {
+      document.querySelector(".moreButton").click();
+    }
+  }
+
+  return true;
+}
+
+function clickMoreButtons() {
+  if (clickNextButton()) {
+    //Keep clicking buttons until there are none in the "update" or "save" states.
+    setTimeout(clickMoreButtons, 100);
+  } else {
+    console.log("donclickingbuttons");
+    console.log(document.querySelector(".venmo-calc-results"));
+    calcStats();
+  }
+}
+
+function getEmojis(str) {
+  return str.match(
+    /(?:[\u2700-\u27bf]|(?:\ud83c[\udde6-\uddff]){2}|[\ud800-\udbff][\udc00-\udfff])[\ufe0e\ufe0f]?(?:[\u0300-\u036f\ufe20-\ufe23\u20d0-\u20f0]|\ud83c[\udffb-\udfff])?(?:\u200d(?:[^\ud800-\udfff]|(?:\ud83c[\udde6-\uddff]){2}|[\ud800-\udbff][\udc00-\udfff])[\ufe0e\ufe0f]?(?:[\u0300-\u036f\ufe20-\ufe23\u20d0-\u20f0]|\ud83c[\udffb-\udfff])?)*/g
+  );
+}
+
+function calcStats() {
+  var recievedArr = [...document.querySelectorAll(".bold.medium.green")];
+  recievedArr = recievedArr.map(el => parseFloat(el.innerText.slice(2)));
+  var sentArr = [...document.querySelectorAll(".bold.medium.red")];
+  sentArr = sentArr.map(el => parseFloat(el.innerText.slice(2)));
+  console.log(recievedArr, sentArr);
+  recievedTotal = recievedArr.reduce((a, b) => a + b).toFixed(2);
+  sentTotal = sentArr.reduce((a, b) => a + b).toFixed(2);
+  console.log(recievedTotal, sentTotal);
+
+  var names = document.querySelectorAll(".m_five_t.p_ten_r");
+  console.log(names);
+  var me = document.querySelector(".rectBox .p_ten_t .bold").innerText;
+  console.log(me);
+  names = [...names].map(a => a.innerText);
+  console.log(names);
+  chargedArr = names.filter(a => a.includes("charged"));
+  console.log(chargedArr);
+
+  const emojiArr = getEmojis(document.querySelector("body").innerText);
+
+  // TODO: Map each transaction to an object
+
+  // const transactionElList = document.querySelectorAll('.profile_feed_story');
+
+  // let transactions = []
+
+  // transactionElList.forEach(el => {
+  //   let newTranction = {};
+  //   newTranction.ammount = Number(el.querySelector('align_top.align_right').innerText.trim().slice(2));
+  //   transactions.push(newTranction);
+  // })
+
+  // console.log(transactions);
+
+  console.log(emojiArr);
+
+  chrome.runtime.sendMessage(
+    {
+      title: "collectionDone",
+      recievedArr,
+      sentArr,
+      recievedTotal,
+      sentTotal,
+      names,
+      me,
+      chargedArr,
+      emojiArr
+    },
+    function(response) {
+      console.dir(response);
+    }
+  );
+}
